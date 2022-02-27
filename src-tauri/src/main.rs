9a95e60b5eb4f65bbf22ce5cdf1bb2b167724dda
k-yomo <k-yomoda@mercari.com>
2022-02-27 17:59:28 +0900

    Add menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,79 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
+
+use tauri::api::{shell};
+use tauri::{
+    CustomMenuItem, Manager, Menu, MenuEntry, MenuItem, Submenu
+};
 
 mod cmd;
 mod app_data;
 
 fn main() {
-  tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![
+    let ctx = tauri::generate_context!();
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
       cmd::get_markdown,
       cmd::save_markdown,
     ])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+        .menu(Menu::with_items([
+            #[cfg(target_os = "macos")]
+                MenuEntry::Submenu(Submenu::new(
+                &ctx.package_info().name,
+                Menu::with_items([
+                    MenuItem::About(ctx.package_info().name.clone()).into(),
+                    MenuItem::Separator.into(),
+                    MenuItem::Services.into(),
+                    MenuItem::Separator.into(),
+                    MenuItem::Hide.into(),
+                    MenuItem::HideOthers.into(),
+                    MenuItem::ShowAll.into(),
+                    MenuItem::Separator.into(),
+                    MenuItem::Quit.into(),
+                ]),
+            )),
+            MenuEntry::Submenu(Submenu::new(
+                "Edit",
+                Menu::with_items([
+                    MenuItem::Undo.into(),
+                    MenuItem::Redo.into(),
+                    MenuItem::Separator.into(),
+                    MenuItem::Cut.into(),
+                    MenuItem::Copy.into(),
+                    MenuItem::Paste.into(),
+                    #[cfg(not(target_os = "macos"))]
+                        MenuItem::Separator.into(),
+                    MenuItem::SelectAll.into(),
+                ]),
+            )),
+            MenuEntry::Submenu(Submenu::new(
+                "View",
+                Menu::with_items([MenuItem::EnterFullScreen.into()]),
+            )),
+            MenuEntry::Submenu(Submenu::new(
+                "Window",
+                Menu::with_items([MenuItem::Minimize.into(), MenuItem::Zoom.into()]),
+            )),
+            // You should always have a Help menu on macOS because it will automatically
+            // show a menu search field
+            MenuEntry::Submenu(Submenu::new(
+                "Help",
+                Menu::with_items([CustomMenuItem::new("Learn More", "Learn More").into()]),
+            )),
+        ]))
+        .on_menu_event(|event| {
+            let event_name = event.menu_item_id();
+            event.window().emit("menu", event_name).unwrap();
+            match event_name {
+                "Learn More" => {
+                    let link = "https://github.com/k-yomo/tod".to_string();
+                    shell::open(&event.window().shell_scope(), link, None).unwrap();
+                }
+                _ => {}
+            }
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
