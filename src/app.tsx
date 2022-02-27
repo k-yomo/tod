a1e934f47f57c56ad81d756180c4b6e253075411
k-yomo <k-yomoda@mercari.com>
2022-02-28 02:02:38 +0900

    Replace editor with CodeMirror and support vim keybinding

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,12 @@
-import {createRef, h} from "preact";
+import {h} from "preact";
 import {useEffect, useState} from "preact/hooks";
-import {Editor} from '@toast-ui/react-editor';
-import {getMarkdown, saveMarkdown} from "./services/storage";
-import {useDebounce} from "./lib/debounce";
-import '@toast-ui/editor/dist/toastui-editor.css';
-import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
+import {getMarkdown} from "./services/storage";
 import './styles/globals.css'
+import Editor from "./components/Editor";
 
 export function App() {
-    const editorRef = createRef();
-    const [markdown, setMarkdown] = useState('')
+    const [markdownText, setMarkdown] = useState("")
     const [loading, setLoading] = useState(true)
-    const debouncedMarkdown = useDebounce(markdown, 1000);
-
-    const onChangeMarkdown = () => {
-        const editor = editorRef.current.editorInst
-        const markdown = editor.getMarkdown();
-        setMarkdown(markdown)
-    }
-
-    const onBlur = () => {
-        saveMarkdown(markdown)
-    }
 
     useEffect(() => {
         getMarkdown().then(markdown => {
@@ -30,29 +15,11 @@ export function App() {
         })
     }, [])
 
-    useEffect(() => {
-        saveMarkdown(debouncedMarkdown)
-    }, [debouncedMarkdown])
-
     if (loading) {
         return <div>loading...</div>
     }
 
     return (
-        <>
-            <Editor
-                initialValue={markdown}
-                initialEditType="markdown"
-                previewStyle="tab"
-                height="100vh"
-                useCommandShortcut
-                hideModeSwitch
-                extendedAutolinks
-                onChange={onChangeMarkdown}
-                onBlur={onBlur}
-                theme='dark'
-                ref={editorRef}
-            />
-        </>
+        <Editor initialText={markdownText} />
     )
 }
